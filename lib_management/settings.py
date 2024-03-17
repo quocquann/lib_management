17652f70d9d744bdaf71ec709b2035f95fda7794
quocquann <nguyenquocquan2233@gmail.com>
2024-03-17 23:36:12 +0700

    add auto schema

--- lib_management/settings.py
+++ lib_management/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
