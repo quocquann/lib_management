159a21e9d8922ca5dcb9775e8dc526ae05bef100
quocquann <nguyenquocquan2233@gmail.com>
2024-03-18 23:25:08 +0700

    edit book model

--- library/models/book.py
+++ library/models/book.py
@@ -9,6 +9,7 @@ class Book(models.Model):
     isbn = models.CharField(max_length=15, unique=True)
     title = models.CharField(max_length=5000)
     image = models.ImageField(upload_to="images", null=None, blank=None)
+    describe = models.CharField(max_length=5000, null=True, blank=True)
 
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
